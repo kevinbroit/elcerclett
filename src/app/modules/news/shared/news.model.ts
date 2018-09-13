cd3e04e21961e1908d50316df3098690466a7cfe
pepinos <pepinos@pepinos.local>
2018-09-13 14:46:08 +0700

    news more layout stuff

--- src/app/modules/news/shared/news.model.ts
+++ src/app/modules/news/shared/news.model.ts
@@ -1,7 +1,11 @@
 export class News {
   constructor(public id: number,
               public title: string,
-              public content: string
+              public content: string,
+              public subtitle : string,
+              public order : number,
+              public visible : boolean,
+              public author : string
               ) {
   }
 
