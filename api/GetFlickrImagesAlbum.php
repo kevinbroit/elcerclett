9d732acecb96af0357ee77b40642c5d8a351f13f
pepinos <pepinos@pepinos.local>
2018-10-01 08:37:04 +0700

    Flickr Update

--- api/GetFlickrImagesAlbum.php
+++ api/GetFlickrImagesAlbum.php
@@ -16,40 +16,40 @@ class Team{
 
 $api_key = '3204bc8768cfd802ecec92c993a53649';
 $user_id='156850972@N06';
-$photoset_id= $_GET['photoset_id']; // TODO GET ARRAY
 $extras='url_t,url_s,url_m,url_l,description'; //url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l
+$albums_id = array('72157700321213521','72157701889611145','72157673968453278','72157700321139941','72157701654772894');
+$teams = array();
 
-$url = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos';
-$url.= '&api_key='.$api_key;
-$url.= '&user_id='.$user_id;
-$url.= '&photoset_id='.$photoset_id;
-$url.= '&extras='.$extras;
-$url.= '&format=json';
-$url.= '&nojsoncallback=1';
+foreach( $albums_id as $photoset_id )
+{
+  $url = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos';
+  $url.= '&api_key='.$api_key;
+  $url.= '&user_id='.$user_id;
+  $url.= '&photoset_id='.$photoset_id;
+  $url.= '&extras='.$extras;
+  $url.= '&format=json';
+  $url.= '&nojsoncallback=1';
+  $response = json_decode(file_get_contents($url));
 
-$response = json_decode(file_get_contents($url));
-$team = $response->photoset->photo;
-$photos =   array();
+  $photos = $response->photoset->photo;
+  $team = new Team();
+  $team->name =$response->photoset->title;
+  $team->players =  array();
 
-echo json_encode($response);
+  foreach($photos as $photo){
+    $player = new Player();
+    $player->name = $photo->title;
+    $player->desc = $photo->description->content;
+    $player->thumbnail = $photo->url_t;
+    $player->image_small = $photo->url_s;
+    $player->image_medium = $photo->url_m;
+    $player->image_large = $photo->url_l;
 
-$team = new Team();
-$team->name =$response->photoset->name;
-$team->players =  array();
-
-foreach($team as $player){
-  $player = new Player();
-  $player->name = $player->title;
-  $player->desc = $player->description;
-  $player->thumbnail = $player->url_t;
-  $player->image_small = $player->url_s;
-  $player->image_medium = $player->url_m;
-  $player->image_large = $player->url_l;
-  array_push($team->players, $player);
+    array_push($team->players, $player);
+  }
+  array_push($teams, $team);
 }
 
-file_put_contents('team_'.$photoset_id.'.json',json_encode($team));
-
+file_put_contents('teams.json',json_encode($teams));
 header('Content-type: application/json');
-
 ?>
